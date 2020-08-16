79160a366d0284f51d4f1193e1a18876cba0b1ba
Vlad Retca <vlad.retca@gmail.com>
2020-08-17 01:49:38 +0300

    bugfix delivery calendar

--- Fespore/Logistia/Block/Adminhtml/Index/Index.php
+++ Fespore/Logistia/Block/Adminhtml/Index/Index.php
@@ -2,16 +2,34 @@
 
 namespace Fespore\Logistia\Block\Adminhtml\Index;
 
+use Magento\Integration\Model\IntegrationFactory;
+
+
 class Index extends \Magento\Backend\Block\Widget\Container
 {
 
+    protected $integrationFactory;
 
-
-    public function __construct(\Magento\Backend\Block\Widget\Context $context,array $data = [])
+    public function __construct(\Magento\Backend\Block\Widget\Context $context, array $data = [],
+                                \Magento\Integration\Model\IntegrationFactory $integrationFactory)
     {
         parent::__construct($context, $data);
+        $this->integrationFactory = $integrationFactory;
     }
 
+
+    function getLogistiaIntegrationStatus()
+    {
+        $integrationExists = $this->integrationFactory->create()->load("Logistia Integration", 'name')->getData();
+        if (empty($integrationExists)) {
+            return false;
+        }
+        if ($integrationExists["status"] == "0") {
+            return false;
+        }
+
+        return true;
+    }
 
 
 }
